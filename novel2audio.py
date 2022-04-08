a71273d7d43243776fda8b0c4d81dccda9840fa6
Zhang Tao <zhangtao103239@buaa.edu.cn>
2022-04-08 13:23:14 +0800

    Update novel2audio.py

--- novel2audio.py
+++ novel2audio.py
@@ -1,7 +1,18 @@
 import getopt
 import sys
+import requests
+
 
 if __name__ == '__main__':
     opts, _ = getopt.getopt(sys.argv[1:], 'u:p:', [""])
     for opt, value in opts:
         print(opt, value)
+
+    url = "https://seafile.ynotme.cn/api2/auth-token/"
+    data = {
+        'username': opts['-u'],
+        'password': opts['-p']
+    }
+    response = requests.request("POST", url, data=data)
+
+    print(response.text)
